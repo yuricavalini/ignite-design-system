6fc614b588e9e5a721464eb36893edb1e80bec46
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-24 07:46:43 -0300

    chore(packages-docs-stories): add Avatar component stories

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/yuricavalini.png',
+    alt: 'Yuri Cavalini',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
