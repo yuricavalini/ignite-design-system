02b226b1f9199a67bf5cf68a28b7b6f61c294a8d
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-27 18:54:36 -0300

    feat(packages-docs-stories): add click action log to Button component stories

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -8,6 +8,11 @@ export default {
   args: {
     children: 'Send',
   },
+  argsTypes: {
+    onClick: {
+      action: 'click',
+    },
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
