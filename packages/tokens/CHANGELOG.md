19ef847f59e0f3eaabe5b642352fc24d5368f142
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-30 21:55:50 -0300

    chore(ignite-design-system): add new color to tokens

---
+++ packages/tokens/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @yuriwlc-ignite-ui/tokens
+
+## 0.1.2
+
+### Minor Changes
+
+- Add new color
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -1,6 +1,7 @@
 export const colors = {
   white: '#FFF',
   black: '#000',
+  orange: '#FF731D',
 
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
